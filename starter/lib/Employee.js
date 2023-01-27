09530b328fc9ee91f432f25fda6f14007a57a2a6
Matthew Osborne <momatthewosborne1@gmail.com>
2023-01-27 18:46:26 +0000

    made first class

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,1 +1,21 @@
 // TODO: Write code to define and export the Employee class
+class employee {
+    constructor(name, id, email) {
+        name = this.name;
+        id = this.id;
+        email = this.id;
+    }
+    getName() {
+        return this.name
+    }
+    getId() {
+        return this.id;
+    }
+    getEmail() {
+        return this.email
+    }
+    getRole() {
+        return "Employee"
+    }
+
+}
